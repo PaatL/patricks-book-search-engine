c0619efd2860c069647f35205bba796847ab8c3d
Patrick <lipatrick093@gmail.com>
2024-03-24 22:01:28 -0400

    test 2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,22 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
 import SearchBooks from './pages/SearchBooks'
 import SavedBooks from './pages/SavedBooks'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+// Create an instance of Apollo Client
+const client = new ApolloClient({
+  uri: 'your_graphql_endpoint_here',
+  cache: new InMemoryCache(),
+});
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: (
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    ),
     errorElement: <h1 className='display-2'>Wrong page!</h1>,
     children: [
       {
--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -7,12 +7,11 @@ import {
   Card,
   Row
 } from 'react-bootstrap';
- 
+import { useMutation } from '@apollo/client'; // Import the useMutation hook
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-import { SAVE_BOOK, ADD_USER, REMOVE_BOOK, LOGIN_USER} from '../utils/mutations';
-import {GET_ME} from '../utils/queries';
+import { SAVE_BOOK } from '../utils/mutations'; // Import the SAVE_BOOK mutation
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -23,15 +22,13 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
-  useMutation(() => {
-    return() => saveBookIds(SAVE_BOOK);
-    
-  })
+  // Define the useMutation hook for saving a book
+  const [saveBookMutation, { error }] = useMutation(SAVE_BOOK);
+
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
-  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
-  });
+  }, [savedBookIds]); // Add savedBookIds to the dependency array
 
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
@@ -78,83 +75,21 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBookMutation({
+        variables: { bookData: bookToSave, username: 'example_username', email: 'example@example.com' }
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      // Handle the response if needed
+      console.log(response);
+
+      // Update savedBookIds state if the book was successfully saved
+      if (response.data) {
+        setSavedBookIds([...savedBookIds, bookToSave.bookId]);
       }
-
-      // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
       console.error(err);
     }
   };
 
-  return (
-    <>
-      <div className="text-light bg-dark p-5">
-        <Container>
-          <h1>Search for Books!</h1>
-          <Form onSubmit={handleFormSubmit}>
-            <Row>
-              <Col xs={12} md={8}>
-                <Form.Control
-                  name='searchInput'
-                  value={searchInput}
-                  onChange={(e) => setSearchInput(e.target.value)}
-                  type='text'
-                  size='lg'
-                  placeholder='Search for a book'
-                />
-              </Col>
-              <Col xs={12} md={4}>
-                <Button type='submit' variant='success' size='lg'>
-                  Submit Search
-                </Button>
-              </Col>
-            </Row>
-          </Form>
-        </Container>
-      </div>
-
-      <Container>
-        <h2 className='pt-5'>
-          {searchedBooks.length
-            ? `Viewing ${searchedBooks.length} results:`
-            : 'Search for a book to begin'}
-        </h2>
-        <Row>
-          {searchedBooks.map((book) => {
-            return (
-              <Col md="4" key={book.bookId}>
-                <Card border='dark'>
-                  {book.image ? (
-                    <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />
-                  ) : null}
-                  <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    <p className='small'>Authors: {book.authors}</p>
-                    <Card.Text>{book.description}</Card.Text>
-                    {Auth.loggedIn() && (
-                      <Button
-                        disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
-                        className='btn-block btn-info'
-                        onClick={() => handleSaveBook(book.bookId)}>
-                        {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
-                          ? 'This book has already been saved!'
-                          : 'Save this Book!'}
-                      </Button>
-                    )}
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
-    </>
-  );
-};
-
+}
 export default SearchBooks;
